6af83998357b2680fb4d9e6ab9ba35b1c17336c0
kadariyaujwal <cooloozewall@gmail.com>
2024-03-28 12:58:18 +0545

    minor

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,7 +10,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ __('App Title') }}</title>
+    <title>{{ __('ENepali') }}</title>
 
     <!-- Scripts -->
 
