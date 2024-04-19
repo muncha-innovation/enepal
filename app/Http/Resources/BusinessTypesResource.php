9f6e5c6d55133d560bc29c18ce2309384ee5e887
kadariyaujwal <cooloozewall@gmail.com>
2024-04-19 23:49:07 +0545

    resource wip

--- app/Http/Resources/BusinessTypesResource.php
+++ app/Http/Resources/BusinessTypesResource.php
@@ -19,40 +19,6 @@ class BusinessTypesResource extends JsonResource
             'title' => $this->title,
             'description' => $this->description,
             'icon' => getImage($this->icon),
-            'businesses' => $this->getBusinesses()
+            'businesses' => BusinessResource::collection($this->businesses->latest()->take(10)->get()),
         ];
-    }
-
-    private function getBusinesses() {
-        return $this->businesses->map(function($business) {
-            return [
-                'id' => $business->id,
-                'title' => $business->title,
-                'description' => $business->description,
-                'image' => getImage($business->image, 'business/'),
-                'address' => $this->getAddress($business),
-                'phone' => $business->phone,
-                'email' => $business->email,
-                'website' => $business->website,
-                'facilities' => $business->facilities->map(function($facility) {
-                    return [
-                        'id' => $facility->id,
-                        'title' => $facility->title,
-                        'icon' => getImage($facility->icon)
-                    ];
-                })
-            ];
-        });
-    }
-
-    private function getAddress($business) {
-        return [
-            'country' => $business->address?->country?->name,
-            'city' => $business->address?->city,
-            'state' => $business->address?->state?->name,
-            'postal_code' => $business->address->postal_code,
-            'address_line_1' => $business->address->address_line_1,
-            'address_line_2' => $business->address->address_line_2,
-        ];
-    }
-}
+    }}
