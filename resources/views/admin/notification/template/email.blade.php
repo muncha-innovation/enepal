b215d71929b33091e17f5773b72660e29178f2ec
kadariyaujwal <cooloozewall@gmail.com>
2024-12-06 17:04:21 +0545

    wip: email template notify

--- resources/views/admin/notification/template/email.blade.php
+++ resources/views/admin/notification/template/email.blade.php
@@ -40,7 +40,7 @@
                     <div class="mb-4">
                         <label for="email_from_email"
                             class="block text-gray-700 text-sm font-bold mb-2">{{ __('Email From Email') }}:</label>
-                        <input type="email" id="email_from_email" name="email_sent_from_email"
+                        <input type="text" id="email_from_email" name="email_sent_from_email"
                             value="{{ $template->email_sent_from_email }}"
                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             required>
