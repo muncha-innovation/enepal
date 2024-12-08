cf0d061af8d20853dbd14df8dbb41fb78714a0bd
kadariyaujwal <cooloozewall@gmail.com>
2024-12-08 18:33:30 +0545

    business custom message

--- resources/views/modules/business/createOrEdit.blade.php
+++ resources/views/modules/business/createOrEdit.blade.php
@@ -208,7 +208,7 @@
                 <div class="mb-2">
                     <label for="custom_email_message"
                         class="block text-sm font-medium leading-6 text-gray-900">{{ __('Custom Email Message') }}</label>
-                    <textarea name="custom_email_message" id="custom_email_message" class="block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="{{ __('This message will be sent to the member in the email') }}"></textarea>
+                    <textarea name="custom_email_message" id="custom_email_message" class="block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="{{ __('This message will be sent to the member in the email') }}">{{ $business->custom_email_message }}</textarea>
                 </div>
                 <div class="mb-2">
                     <label for="active"
