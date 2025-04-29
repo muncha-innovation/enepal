fd32b32ea72ba8417052907d39b64bb7f157c4f4
kadariyaujwal <cooloozewall@gmail.com>
2025-04-29 09:22:06 +0545

    Update JavaScript files and dependencies
    - Added license information for Pusher JavaScript Library in app.js.LICENSE.txt.
    - Refactored communications.js to remove unnecessary comments and optimize code.
    - Updated mix-manifest.json with new version hashes for app.js and communications.js.
    - Updated thread-management.js script reference in index.blade.php to include versioning.

--- resources/views/modules/business/communications/index.blade.php
+++ resources/views/modules/business/communications/index.blade.php
@@ -309,7 +309,7 @@
 @push('js')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<script src="{{ asset('js/thread-management.js') }}"></script>
+<script src="{{ asset('js/thread-management.js?v=1.0.1') }}"></script>
 <script src="{{ asset('js/communications.js') }}"></script>
 <script>
   $(document).ready(function() {
