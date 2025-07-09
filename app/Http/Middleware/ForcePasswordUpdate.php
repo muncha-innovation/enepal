65240fba6bcd8c99003efd48e3a2fc77e13b0c52
kadariyaujwal <cooloozewall@gmail.com>
2025-07-09 11:15:04 +0545

    redirect loop fix?

--- app/Http/Middleware/ForcePasswordUpdate.php
+++ app/Http/Middleware/ForcePasswordUpdate.php
@@ -10,7 +10,7 @@ class ForcePasswordUpdate
     public function handle(Request $request, Closure $next)
     {
         // Check if user is authenticated
-        if ($request->user() && $request->user()->force_update_password && !$request->is('profile/password')) {
+        if ($request->user() && $request->user()->force_update_password && !$request->is('profile*')) {
             // Redirect to the profile page with a session value to show the security tab
             return redirect()->route('profile')->with('active_profile_tab', 'security');
         }
